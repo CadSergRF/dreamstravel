06fa3b7ab4fbe9a133ad0b408517f42f86cc60e0
Sergey Evdokimov <cadsergrf@yandex.ru>
2022-11-04 00:05:49 +0700

    added js

---
+++ script.js
@@ -0,0 +1,18 @@
+document.querySelector('.header__btn').addEventListener('click', function () {
+  const complain = document.querySelector('.complain');
+  complain.classList.removeClass('complain__close');
+})
+
+
+
+
+
+// document.querySelector('button').addEventListener('click', function () {
+
+//   document.querySelector('header__btn').on('click', function () {
+//     document.querySelector('complain').removeClass('complain__close');
+//   });
+//   document.querySelector('complain__close-btn').on('click', function () {
+//     document.querySelector('complain').addClass('complain__close');
+//   });
+// })
