a6579aa2f85257e2d5a74f7bbbc807ca6aff4357
Sergey Evdokimov <cadsergrf@yandex.ru>
2022-11-04 01:22:21 +0700

    work js

--- script.js
+++ script.js
@@ -1,18 +1,9 @@
-document.querySelector('.header__btn').addEventListener('click', function () {
-  const complain = document.querySelector('.complain');
-  complain.classList.removeClass('complain__close');
+document.querySelector('button.header__link').addEventListener('click', function () {
+  
+  document.querySelector('.complain').classList.remove('complain__close');
 })
 
-
-
-
-
-// document.querySelector('button').addEventListener('click', function () {
-
-//   document.querySelector('header__btn').on('click', function () {
-//     document.querySelector('complain').removeClass('complain__close');
-//   });
-//   document.querySelector('complain__close-btn').on('click', function () {
-//     document.querySelector('complain').addClass('complain__close');
-//   });
-// })
+document.querySelector('.complain__close-btn').addEventListener('click', function () {
+  
+  document.querySelector('.complain').classList.add('complain__close');
+})
